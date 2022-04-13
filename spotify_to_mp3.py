b5f3c6f0d40a33f817717d5b6d1a255aa45da70d
NicholasWang <nicholas.x.wang@gmail.com>
2022-04-12 20:10:57 -0700

    got it to work

--- spotify_to_mp3.py
+++ spotify_to_mp3.py
@@ -1,20 +1,49 @@
-# Downloads a Spotify playlist into a folder of MP3 tracks
-# Jason Chen, 21 June 2020
-
 import os
+from os import system, name
 import spotipy
 import spotipy.oauth2 as oauth2
 import youtube_dl
 from youtube_search import YoutubeSearch
 
-# **************PLEASE READ THE README.md FOR USE INSTRUCTIONS**************
+def clear_terminal():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def convert_to_mp3(folder):
+    from os import walk
+    f = []
+    for (dirpath, dirnames, filenames) in walk(folder):
+        f.extend(filenames)
+        break
+    endings = {}
+    for i in f:
+        splitted = i.split(".")
+        try:
+            endings[splitted[-1].lower()] += 1
+        except:
+            endings[splitted[-1].lower()] = 1
+    print(endings)
+    from pydub import AudioSegment
+    for i in f:
+        ending = i.split(".")[-1].lower()
+        if ending == "part" or ending == "ds_store" or ending == "txt":
+            continue
+        print("Making " + i)
+        audio = AudioSegment.from_file("./Pop Music (Winter 2021)/" + i, format=ending)
+        audio.export("./MP3-Music/" + i.replace(ending, "mp3"), format="mp3")
 
 def write_tracks(text_file, tracks):
 
     # Writes the information of all tracks in the playlist to a text file. 
     # This includins the name, artist, and spotify URL. Each is delimited by a comma.
     text_file = str(text_file)
-    tracks = dict(tracks) 
+    tracks = dict(tracks)
     with open(text_file, 'w+', encoding='utf-8') as file_out:
         while True:
             for item in tracks['items']:
@@ -47,7 +76,7 @@ def write_playlist(username, playlist_id):
     playlist_id = str(playlist_id)
     results = spotify.user_playlist(username, playlist_id, fields='tracks,next,name')
     playlist_name = results['name']
-    text_file = u'{0}.txt'.format(playlist_name, ok='-_()[]{}')
+    text_file = "songs.txt"
     print(u'Writing {0} tracks to {1}.'.format(results['tracks']['total'], text_file))
     tracks = results['tracks']
     write_tracks(text_file, tracks)
@@ -59,6 +88,7 @@ def find_and_download_songs(reference_file):
     TOTAL_ATTEMPTS = 10
     with open(reference_file, "r", encoding='utf-8') as file:
         for line in file:
+            clear_terminal()
             temp = line.split(",")
             name, artist = temp[0], temp[1]
             text_to_search = artist + " - " + name
@@ -95,19 +125,22 @@ def find_and_download_songs(reference_file):
 
 if __name__ == "__main__":
     # Parameters
-    print("Please read README.md for use instructions.")    
-    client_id = "89af8ca1688d4104a64e8f8e65d2ddae"
-    client_secret = "b433607ebfb44dc6bb1ecd3e2d14d723"
-    username = "	kkbp42dkp4hweuogt99r8t8wf"
-    playlist_uri = "4uT2sjCdhY30NBMktykuLO"
+    client_id = "846095b9ce934b0da3e0aaf3adbf600c"
+    client_secret = "1d79c77cee124d8f8e20b16f720d65e8"
+    username = "kkbp42dkp4hweuogt99r8t8wf"
+    playlist_uri = "5AbjzbPFE7rMP2ndFqd6mT"
     auth_manager = oauth2.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     playlist_name = write_playlist(username, playlist_uri)
-    reference_file = "{}.txt".format(playlist_name)
-    # Create the playlist folderm
-    if not os.path.exists(playlist_name):
-        os.makedirs(playlist_name)
-    os.rename(reference_file, playlist_name + "/" + reference_file)
-    os.chdir(playlist_name)
+    reference_file = "{}.txt".format("songs")
+    # Create the playlist folder
+    if not os.path.exists("songs"):
+        os.makedirs("songs")
+    os.rename(reference_file, "songs" + "/" + reference_file)
+    os.chdir("songs")
     find_and_download_songs(reference_file)
+    clear_terminal()
+    print("Converting to MP3...")
+    convert_to_mp3("songs")
+    clear_terminal()
     print("Operation complete.")
